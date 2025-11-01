ae7ba1cbd2521b00ba3d8d418e18784b72e56954
Olexandr <olexandrkirichuk@gmail.com>
2025-11-01 11:13:13 +0200

    upd redirect

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Auth\Controllers\AuthenticatedSessionController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('admin/login');
 });
 require __DIR__ . '/admin.php';
 
